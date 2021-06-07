58feb30b579c88d89e9a46776c7940833cc029b6
Виталий Баник <cefiro666@mail.ru>
2021-06-07 13:53:26 +0700

    Обновил скрипт и зависимость

--- updateDependencies/build.sh
+++ updateDependencies/build.sh
@@ -3,13 +3,10 @@
 # stop script when errors
 set -e
 
-# save BUILD_DIRECTORY path
+# save paths
 export BUILD_DIRECTORY=$(pwd)
-
 cd ../
-
 export PROJECT_DIRECTORY=$(pwd)
-
 cd $BUILD_DIRECTORY
 
 # create temp directory
@@ -20,9 +17,8 @@ cd tempBuild
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$BUILD_DIRECTORY/tempBuild/depot_tools:$PATH
 
-# get mediasoup-ios-client
-git clone --recurse-submodules https://github.com/sequenia/mediasoup-ios-client.git
-
+# get repo with dependencies
+git clone https://github.com/sequenia/mediasoup-ios-client.git
 git clone https://github.com/sequenia/libmediasoupclient.git mediasoup-ios-client/mediasoup-client-ios/dependencies/libmediasoupclient
 
 # get the WebRTC iOS code
@@ -34,7 +30,7 @@ cd src
 git checkout -b m84 refs/remotes/branch-heads/4147
 gclient sync
 
-# replace find_sdk.py with correct script
+# replace files with correct
 cp $BUILD_DIRECTORY/supportFiles/find_sdk.py $BUILD_DIRECTORY/tempBuild/webrtc-ios/src/build/mac/
 
 # without bitcode
@@ -50,17 +46,9 @@ mkdir universal
 # create a FAT libwebrtc static library
 lipo -create arm64_libs/obj/libwebrtc.a -output universal/libwebrtc.a
 
-cp -r $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src/ $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/webrtc/src
-
-cp -r $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/libmediasoupclient/* $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/libmediasoupclient/
-
-# copy library file
-cp -r $BUILD_DIRECTORY/tempBuild/webrtc-ios/src/out_ios_libs $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src/
-cp -r $BUILD_DIRECTORY/tempBuild/webrtc-ios/src/out_ios_libs $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/webrtc/src
-
 cd $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/
 
-mv $BUILD_DIRECTORY/tempBuild/webrtc-ios/src/third_party $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src
+mv $BUILD_DIRECTORY/tempBuild/webrtc-ios/src/* $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src/
 
 # build iOS arm64
 cmake . -Bbuild -DLIBWEBRTC_INCLUDE_PATH=$BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src -DLIBWEBRTC_BINARY_PATH=$BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src/out_ios_libs/universal -DMEDIASOUP_LOG_TRACE=ON -DMEDIASOUP_LOG_DEV=ON -DCMAKE_CXX_FLAGS="-fvisibility=hidden" -DLIBSDPTRANSFORM_BUILD_TESTS=OFF -DIOS_SDK=iphone -DIOS_ARCHS="arm64" -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/
@@ -79,7 +67,10 @@ mkdir $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/libmediasoupclient/li
 cp libmediasoupclient/lib/libmediasoupclient.a $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/libmediasoupclient/lib/
 cp libmediasoupclient/lib/libsdptransform.a $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/libmediasoupclient/lib/
 
-mv $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src/third_party $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/webrtc/src
+cp -r $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/libmediasoupclient/* $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/libmediasoupclient/
+
+mv $BUILD_DIRECTORY/tempBuild/mediasoup-ios-client/mediasoup-client-ios/dependencies/webrtc/src/* $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/webrtc/src/
+cp $BUILD_DIRECTORY/supportFiles/byte_order.h $PROJECT_DIRECTORY/mediasoup-client-ios/dependencies/webrtc/src/rtc_base/
 
 # clear temp
 # rm -rf $BUILD_DIRECTORY/tempBuild
